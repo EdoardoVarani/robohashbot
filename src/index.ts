7348166d8b368337c799207ee03184859105337c
Edoardo Varani <edoardo.varani@gmail.com>
2020-05-11 19:03:50 +0200

    default path handler

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,19 @@ export function handleRobo(req: Request, res: Response) {
     const chat = req.body.message.chat;
     const bot = new TelegramBot(TG_TOKEN);
 
+    if(req.originalUrl.includes(TG_TOKEN)){
         bot.sendPhoto(chat.id, ROBOHASH_URL+"/"+req.body.message.text);
-
-    res
-        .status(200)
-        .type('application/json')
-        .send("OK telegram")
-        .end
+        res
+            .status(200)
+            .type('application/json')
+            .send('ACK telegram')
+            .end
+    } else {
+        res
+            .status(200)
+            .type('html')
+            .send('<h2> hello from gcp robohashbot</h2>')
+            .end
+    }
     
 }
